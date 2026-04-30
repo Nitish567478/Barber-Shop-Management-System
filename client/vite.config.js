ddad1d13a6153e9b10d8421fb5871190a3f957f8
Nitish <kumarnitsh147grd@gmail.com>
2026-04-30 23:19:48 +0530

    Solution

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://barber-shop-management-system-1.onrender.com',
         changeOrigin: true,
       },
     },
